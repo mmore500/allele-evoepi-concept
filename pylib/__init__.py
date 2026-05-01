fcfbb64e95279d9f16385955575266c8c3e1bd5a
mmore500 <mmore500@users.noreply.github.com>
2026-05-01 00:20:00 +0000

    Deploying to bindle from @ mmore500/allele-evoepi-concept@095b28e1bd039b08cb16298e8f3243964ff5d483 🚀

--- pylib/__init__.py
+++ pylib/__init__.py
@@ -3,8 +3,10 @@ __email__ = "m.more500@gmail.com"
 __version__ = "0.0.0"
 
 from . import abm_2026_03_16  # noqa: F401
+from ._draw_scatter_tree import draw_scatter_tree
 from ._rescale_stacked_kdeplot import rescale_stacked_kdeplot
 
 __all__ = [
+    "draw_scatter_tree",
     "rescale_stacked_kdeplot",
 ]
