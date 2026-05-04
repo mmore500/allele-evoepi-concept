c71def5a1f82500a051ebc6a850b1798cdc6dea4
mmore500 <mmore500.login@gmail.com>
2026-05-04 06:00:45 +0000

    Deploying to bindle-2026-05-03-basictable2-strain-curves.py from @ mmore500/allele-evoepi-concept@b1d2f9139dee3e969507838c140ed90dfccfad41 🚀

--- bindle/2026-05-03-basictable2-strain-curves.py
+++ bindle/2026-05-03-basictable2-strain-curves.py
@@ -28,7 +28,8 @@ def import_pkg():
 
 @app.cell(hide_code=True)
 def do_watermark(mo, watermark):
-    mo.md(f"""
+    mo.md(
+        f"""
     ```Text
     {watermark(
         current_date=True,
@@ -40,7 +41,8 @@ def do_watermark(mo, watermark):
         globals_=globals(),
     )}
     ```
-    """)
+    """
+    )
     return
 
 
