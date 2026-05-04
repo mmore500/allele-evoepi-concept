91ead327beb2ff20c36ba7c2b096ff9ea8115426
mmore500 <mmore500@users.noreply.github.com>
2026-05-04 02:07:55 +0000

    Deploying to bindle from @ mmore500/allele-evoepi-concept@ca509823a1079bad2eaf0085e911f1a0bd20f6e5 🚀

--- pylib/__init__.py
+++ pylib/__init__.py
@@ -5,9 +5,10 @@ __version__ = "0.0.0"
 from . import abm_2026_03_16  # noqa: F401
 from ._draw_scatter_tree import draw_scatter_tree
 from ._rescale_stacked_kdeplot import rescale_stacked_kdeplot
-from ._strain_palette import strain_palette
+from ._strain_palette import allele_palette, strain_palette
 
 __all__ = [
+    "allele_palette",
     "draw_scatter_tree",
     "rescale_stacked_kdeplot",
     "strain_palette",
